882189c983d5660cbc3f9228e5fbf00a85f5ba09
MUHAMMEDHASHIMCP <cpmohdhashim@gmail.com>
2022-11-23 13:16:22 +0530

    mui lama social media

--- src/App.js
+++ src/App.js
@@ -1,125 +1,35 @@
-import React from 'react';
-import {
-	Typography,
-	AppBar,
-	Card,
-	CardActions,
-	CardContent,
-	CardMedia,
-	CssBaseline,
-	Grid,
-	Toolbar,
-	Container,
-	Button,
-} from '@material-ui/core';
-import CameraIcon from '@material-ui/icons/PhotoCamera';
-import useStyles from './styles';
+import Sidebar from './components/Sidebar';
+import Feed from './components/Feed';
+import Rightbar from './components/Rightbar';
+import { Box, createTheme, Stack, ThemeProvider } from '@mui/material';
+import Navbar from './components/Navbar';
+import Add from './components/Add';
+import { useState } from 'react';
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 function App() {
-	const classes = useStyles();
+	const [mode, setMode] = useState('light');
+
+	const darkTheme = createTheme({
+		palette: {
+			mode: mode,
+		},
+	});
 	return (
-		<>
-			<CssBaseline />
-			<AppBar position="relative">
-				<Toolbar>
-					<CameraIcon />
-					<Typography variant="h6">Photo Album</Typography>
-				</Toolbar>
-			</AppBar>
-			<main>
-				<div className={classes.heroContent}>
-					<Container maxWidth="sm">
-						<Typography
-							component="h1"
-							variant="h2"
-							align="center"
-							color="textPrimary"
-							gutterBottom
-						>
-							Album layout
-						</Typography>
-						<Typography
-							variant="h5"
-							align="center"
-							color="textSecondary"
-							paragraph
-						>
-							Something short and leading about the collection
-							below—its contents, the creator, etc. Make it short
-							and sweet, but not too short so folks don't simply
-							skip over it entirely.
-						</Typography>
-						<div className={classes.heroButtons}>
-							<Grid container spacing={2} justify="center">
-								<Grid item>
-									<Button variant="contained" color="primary">
-										Main call to action
-									</Button>
-								</Grid>
-								<Grid item>
-									<Button variant="outlined" color="primary">
-										Secondary action
-									</Button>
-								</Grid>
-							</Grid>
-						</div>
-					</Container>
-				</div>
-				<Container className={classes.cardGrid} maxWidth="md">
-					<Grid container spacing={4}>
-						{cards.map((card) => (
-							<Grid item key={card} xs={12} sm={6} md={4}>
-								<Card className={classes.card}>
-									<CardMedia
-										className={classes.cardMedia}
-										image="https://source.unsplash.com/random"
-										title="Image title"
-									/>
-									<CardContent
-										className={classes.cardContent}
-									>
-										<Typography
-											gutterBottom
-											variant="h5"
-											component="h2"
-										>
-											Heading
-										</Typography>
-										<Typography>
-											This is a media card. You can use
-											this section to describe the
-											content.
-										</Typography>
-									</CardContent>
-									<CardActions>
-										<Button size="small" color="primary">
-											View
-										</Button>
-										<Button size="small" color="primary">
-											Edit
-										</Button>
-									</CardActions>
-								</Card>
-							</Grid>
-						))}
-					</Grid>
-				</Container>
-			</main>
-			<footer className={classes.footer}>
-				<Typography variant="h6" align="center" gutterBottom>
-					Footer
-				</Typography>
-				<Typography
-					variant="subtitle1"
-					align="center"
-					color="textSecondary"
-					component="p"
+		<ThemeProvider theme={darkTheme}>
+			<Box bgcolor={'background.default'} color={'text.primary'}>
+				<Navbar />
+				<Stack
+					direction="row"
+					spacing={2}
+					justifyContent="space-between"
 				>
-					Something here to give the footer a purpose!
-				</Typography>
-			</footer>
-		</>
+					<Sidebar setMode={setMode} mode={mode} />
+					<Feed />
+					<Rightbar />
+				</Stack>
+				<Add /> 
+			</Box>
+		</ThemeProvider>
 	);
 }
 
